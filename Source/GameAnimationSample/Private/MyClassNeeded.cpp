5d054c66d65b764c141f0962319f686a499a5b67
Rodrigo Schott <rodrigo.schott@gmail.com>
2025-04-13 07:16:22 -0300

    Auto-commit: Modificações em Source

---
+++ Source/GameAnimationSample/Private/MyClassNeeded.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyClassNeeded.h"
+
+MyClassNeeded::MyClassNeeded()
+{
+}
+
+MyClassNeeded::~MyClassNeeded()
+{
+}
